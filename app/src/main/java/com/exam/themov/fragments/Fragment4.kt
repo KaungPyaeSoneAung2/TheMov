fbadec688c6df16a0f035002f536a6197e552fb5
NeyThuAung34 <naythuaung037@gmail.com>
2022-08-29 09:28:44 +0630

    Final Update1

---
+++ app/src/main/java/com/exam/themov/fragments/Fragment4.kt
@@ -0,0 +1,30 @@
+package com.exam.themov.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.exam.themov.R
+
+
+class Fragment4 : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_4, container, false)
+    }
+
+
+}
