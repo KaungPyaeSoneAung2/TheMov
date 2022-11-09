5403d8497928ecf16066046ea18c0fecd12e3ed7
KaungPyaeSoneAung2 <kaungpyae.kpp16@gmail.com>
2022-11-09 22:55:31 +0630

    Splash Screen Added

---
+++ app/src/main/java/com/exam/themov/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.exam.themov
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },3000)
+
+    }
+}
